1e0e2c8adf1d5de88aa15878114ad76caa5c0c7c
Arata Tanaka <ky.37f.9850@s.thers.ac.jp>
2025-07-14 00:40:36 +0900

    update

--- src/model/backbone/ev_gnn.py
+++ src/model/backbone/ev_gnn.py
@@ -36,5 +36,8 @@ class EVGNN(nn.Module):
         effective_radius = 2*float(int(cfg.ev_graph.radius * width + 2) / width)
         self.edge_attrs = Cartesian(norm=True, cat=False, max_value=effective_radius)
 
-    def forward(self, x: Data):
-        pass
+    def forward(self, x: Data, reset=True):
+        if hasattr(data, 'reset'):
+            reset = data.reset
+
+        data = self.events_to_graph(data, reset=reset)
