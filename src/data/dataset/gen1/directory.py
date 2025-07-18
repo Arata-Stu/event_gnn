b3368280939c59fc07e02692c5508fc7c14de48e
Tanaka Arata <ky.37f.9850@s.thers.ac.jp>
2025-07-18 09:43:26 +0900

    change directory path

--- src/data/dataset/gen1/directory.py
+++ src/data/dataset/gen1/directory.py
@@ -4,6 +4,6 @@ class Gen1Directory:
     
     def __init__(self, h5_path: Path):
         self.root: Path = h5_path.parent
-        self.name: str = h5_path.name.removesuffix('.dat.h5')
+        self.name: str = h5_path.name.removesuffix('_td.dat.h5')
         self.event_file: Path = h5_path
         self.track_file: Path = self.root / f"{self.name}_bbox.npy"
